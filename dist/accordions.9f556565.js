8b6355ad798d41ec08a1d4507b95ff98af6273a1
dragan002 <draganvujic29@gmail.com>
2024-04-26 17:03:56 +0200

    Accordion functionality work

--- dist/accordions.9f556565.js
+++ dist/accordions.9f556565.js
@@ -118,43 +118,25 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"resources/js/accordions.js":[function(require,module,exports) {
-function shiftLeft() {
-  var boxes = document.querySelectorAll(".box");
-  var tmpNode = boxes[0];
-  boxes[0].className = "box move-out-from-left";
-  setTimeout(function () {
-    if (boxes.length > 5) {
-      tmpNode.classList.add("box--hide");
-      boxes[5].className = "box move-to-position5-from-left";
-    }
-    boxes[1].className = "box move-to-position1-from-left";
-    boxes[2].className = "box move-to-position2-from-left";
-    boxes[3].className = "box move-to-position3-from-left";
-    boxes[4].className = "box move-to-position4-from-left";
-    boxes[0].remove();
-    document.querySelector(".cards__container").appendChild(tmpNode);
-  }, 500);
-}
-function shiftRight() {
-  var boxes = document.querySelectorAll(".box");
-  boxes[4].className = "box move-out-from-right";
-  setTimeout(function () {
-    var noOfCards = boxes.length;
-    if (noOfCards > 4) {
-      boxes[4].className = "box box--hide";
-    }
-    var tmpNode = boxes[noOfCards - 1];
-    tmpNode.classList.remove("box--hide");
-    boxes[noOfCards - 1].remove();
-    var parentObj = document.querySelector(".cards__container");
-    parentObj.insertBefore(tmpNode, parentObj.firstChild);
-    tmpNode.className = "box move-to-position1-from-right";
-    boxes[0].className = "box move-to-position2-from-right";
-    boxes[1].className = "box move-to-position3-from-right";
-    boxes[2].className = "box move-to-position4-from-right";
-    boxes[3].className = "box move-to-position5-from-right";
-  }, 500);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var accordionButtons = document.querySelectorAll(".product__accordion");
+  accordionButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      // Toggle the active class on the clicked button
+      this.classList.toggle("active");
+
+      // Get the panel associated with the clicked button
+      var panel = this.nextElementSibling;
+
+      // Toggle the max-height style to show/hide the panel
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  });
+});
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -180,7 +162,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58479" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50283" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- resources/js/accordions.js
+++ resources/js/accordions.js
@@ -1,44 +1,20 @@
-function shiftLeft() {
-  const boxes = document.querySelectorAll(".box");
-  const tmpNode = boxes[0];
-  boxes[0].className = "box move-out-from-left";
+document.addEventListener("DOMContentLoaded", function() {
+  const accordionButtons = document.querySelectorAll(".product__accordion");
 
-  setTimeout(function() {
-      if (boxes.length > 5) {
-          tmpNode.classList.add("box--hide");
-          boxes[5].className = "box move-to-position5-from-left";
-      }
-      boxes[1].className = "box move-to-position1-from-left";
-      boxes[2].className = "box move-to-position2-from-left";
-      boxes[3].className = "box move-to-position3-from-left";
-      boxes[4].className = "box move-to-position4-from-left";
-      boxes[0].remove();
+  accordionButtons.forEach(button => {
+      button.addEventListener("click", function() {
+          // Toggle the active class on the clicked button
+          this.classList.toggle("active");
 
-      document.querySelector(".cards__container").appendChild(tmpNode);
+          // Get the panel associated with the clicked button
+          const panel = this.nextElementSibling;
 
-  }, 500);
-
-}
-
-function shiftRight() {
-  const boxes = document.querySelectorAll(".box");
-  boxes[4].className = "box move-out-from-right";
-  setTimeout(function() {
-      const noOfCards = boxes.length;
-      if (noOfCards > 4) {
-          boxes[4].className = "box box--hide";
-      }
-
-      const tmpNode = boxes[noOfCards - 1];
-      tmpNode.classList.remove("box--hide");
-      boxes[noOfCards - 1].remove();
-      let parentObj = document.querySelector(".cards__container");
-      parentObj.insertBefore(tmpNode, parentObj.firstChild);
-      tmpNode.className = "box move-to-position1-from-right";
-      boxes[0].className = "box move-to-position2-from-right";
-      boxes[1].className = "box move-to-position3-from-right";
-      boxes[2].className = "box move-to-position4-from-right";
-      boxes[3].className = "box move-to-position5-from-right";
-  }, 500);
-
-}
+          // Toggle the max-height style to show/hide the panel
+          if (panel.style.maxHeight) {
+              panel.style.maxHeight = null;
+          } else {
+              panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+      });
+  });
+});
