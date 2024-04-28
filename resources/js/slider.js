40140b6666d771bbfc7f01b336f484dcaef65048
dragan002 <draganvujic29@gmail.com>
2024-04-28 12:27:59 +0200

    Working on swiper

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -1,24 +1,18 @@
-    // Selecting the scroll container using the correct class selector
-let scrollContainer = document.querySelector('.product__gallery');
-let backBtn = document.getElementById("backBtn");
-let nextBtn = document.getElementById("nextBtn");
-
-// Attaching the event listener to the window object to capture all scroll events
-window.addEventListener("wheel", (evt) => {
-    // Checking if the scroll event occurred within the scroll container
-    if (scrollContainer.contains(evt.target)) {
-        evt.preventDefault();
-        scrollContainer.scrollLeft += evt.deltaY;
-        scrollContainer.style.scrollBehavior = "auto"; // Fixed the typo
-    }
-});
-
-nextBtn.addEventListener('click', () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += scrollContainer.clientWidth; // Adjusting scroll position
-});
-
-backBtn.addEventListener('click', () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= scrollContainer.clientWidth; // Adjusting scroll position
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
 });
